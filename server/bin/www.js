c868fd0c20d9584bb65aa832ea7492527003e25b
amanda.quinto <amanda.quinto@br.clara.net>
2018-12-01 13:13:32 -0200

    Aula 6

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,8 +1,15 @@
 // server/bin/www.js
 
 const app = require('../app')
+const cluster = require('cluster')
+const numCpus = require('os').cpus().length
 
+if (cluster.isMaster){
+    for (let i = 0; i<numCpus; i++){
+        cluster.fork()
+    }
+}else{
 app.listen(3000, () => {
     console.log('server is up!!')
 })
-
+}
